71170590d79ea6a321f42d68ff917adcefffc93b
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-05-01 14:37:46 -0500

    database-exercises

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   artist VARCHAR(35) NOT NULL,
+   name VARCHAR(35) NOT NULL,
+   release_date VARCHAR(35) NOT NULL,
+   sales VARCHAR(255) NOT NULL,
+   genre VARCHAR(35) NOT NULL,
+   PRIMARY KEY (id)
+
+);
