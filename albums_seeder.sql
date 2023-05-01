556d778b38342347f9d47d740aca5887d1c17ecc
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-05-01 16:25:41 -0500

    Albums Update

---
+++ albums_seeder.sql
@@ -0,0 +1,42 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+    VALUES
+('AC/DC','Back in Black','1980','30.1','Hard rock'),
+('Whitney Houston','The Bodyguard','1992','28.7','R&B'),
+('Pink Floyd','The Dark Side of the Moon','1973','24.8','Progressive rock'),
+('Eagles','Their Greatest Hits (1971–1975)','1976','41.2','Country rock'),
+('Meat Loaf','Bat Out of Hell','1977','22','Hard rock'),
+('Eagles','Hotel California','1976','31.8','Soft rock'),
+('Shania Twain','Come On Over',1997,'30.4','Country'),
+('Fleetwood Mac','Rumours',1977,29.3 ,'Soft rock'),
+('Bee Gees','Saturday Night Fever', 1977, 22.1,'Disco'),
+('Led Zeppelin', 'Led Zeppelin IV', 1971,  30.4,'Hard rock'),
+('Michael Jackson', 'Bad', 1987, 21.2, 'Pop, rhythm and blues, funk and rock'),
+('Alanis Morissette', 'Jagged Little Pill', 1995,25.4, 'Alternative rock'),
+('Various artists', 'Dirty Dancing', 1987, 24.1, 'Pop'),
+('Celine Dion', 'Falling into You', 1996,  21.1,'Pop'),
+('Adele', '21', 2011, 27.1,'Pop, soul'),
+('The Beatles', '1', 2000, 23.2,'Rock'),
+('Metallica', 'Metallica', 1991, 22.7,'Heavy metal'),
+('Celine Dion', 'Let''s Talk About Love',1997,20.5, 'Pop'),
+('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers', 1984,22.9, 'Reggae'),
+('Guns N'' Roses', 'Appetite for Destruction', 1987,22.8, 'Hard rock'),
+('Bruce Springsteen', 'Born in the U.S.A.', 1984,22, 'Heartland rock'),
+('ABBA', 'Gold: Greatest Hits', 1992, 21.6,'Pop, disco'),
+('Dire Straits', 'Brothers in Arms', 1985, 21.1,'Roots rock, blues rock, soft rock'),
+('Santana', 'Supernatural', 1999, 20.8 ,'Latin rock'),
+('Madonna', 'The Immaculate Collection', 1990, 19.8,'Pop, dance'),
+('Pink Floyd', 'The Wall', 1979, 18.9,'Progressive rock'),
+('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.3 ,'Rock'),
+('Michael Jackson', 'Dangerous', 1991, 17.0, 'New jack swing'),
+('The Beatles', 'Abbey Road', 1969, 16.9, 'Rock');
+
+
+
+
+
+
+
+
+
+
