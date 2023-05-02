ec2992043e70dc6423eaae8159e5811249ac26fa
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-05-02 10:57:12 -0500

    database-exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,9 +1,10 @@
 USE codeup_test_db;
 
-# SELECT name FROM albums WHERE release_date > 1991;
-# SELECT * FROM albums WHERE genre = 'Disco';
-# SELECT * FROM albums WHERE artist = 'Whitney Houston';
+SELECT name FROM albums WHERE release_date > 1991;
+SELECT * FROM albums WHERE genre = 'Disco';
+SELECT * FROM albums WHERE artist = 'Whitney Houston';
 
 DELETE FROM albums WHERE release_date > 1991;
 DELETE FROM albums WHERE genre = 'Disco';
 DELETE FROM albums WHERE artist = 'Whitney Houston';
+
